059be9510b7b8c6deb93740aa5183cd6955cbbed
ManuelSanchezCaba <mfsanchezcaba@gmail.com>
2019-11-22 11:01:37 -0400

    Adding functionality to create, view, delete, and update a user

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,5 +12,5 @@ app.use(require('./routes/authentication'));
 
 //Starting the servers
 app.listen(app.get('port'), () => {
-    console.log('Server on port 4000', app.get('port'));
+    console.log('Server on port', app.get('port'));
 });
--- backend/src/routes/authentication.js
+++ backend/src/routes/authentication.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const mysqlConnection = require('../database');
 
-router.get('/', (req, res) => {
+//Esta funcion busca los usuarios registrados
+router.get('/authentication', (req, res) => {
       mysqlConnection.query("select * from authentication", (err, rows, fields) => {
           if(!err) {
               res.json(rows);
@@ -11,6 +12,55 @@ router.get('/', (req, res) => {
               console.log(err);
           }
       });
+});
+
+//Esta funcion busca un usuario registrado por el id
+router.get('/authentication/:id', (req, res) => {
+      const { id } = req.params;
+      mysqlConnection.query("select * from authentication where idAuthentication = ?", [id], (err, rows, fields) => {
+          if(!err) {
+              res.json(rows[0]);
+          } else {
+              console.log(err);
+          }
+      });
+});
+
+//Esta funcion crea un usuario
+router.post('/authentication', (req, res) => {
+      const { user, password, nombre, tipo_usuario, email } = req.body;
+      mysqlConnection.query("insert into authentication(user, password, nombre, tipo_usuario, email) values ('" + user + "', '" + password + "', '" + nombre + "', '" + tipo_usuario + "', '" + email + "')", (err, rows, fields) => {
+            if(!err) {
+                  res.json({status: 'User Saved'});
+              } else {
+                  console.log(err);
+              }
+      });
+});
+
+//Esta funcion actualiza un usuario
+router.put('/authentication/:id', (req, res) => {
+      const user = req.body;
+      const { id } = req.params;
+      mysqlConnection.query("update authentication set ? where idAuthentication = ?", [user, id], (err, rows, fields) => {
+            if(!err) {
+                  res.json({status: 'User Updated'});
+              } else {
+                  console.log(err);
+              }
+      });
+});
+
+//Esta funcion elimina un usuario
+router.delete("/authentication/:id", (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query("delete from authentication where idAuthentication = ?", [id], (err, rows, fields) => {
+        if(!err) {
+              res.json({status: 'User Deleted'});
+          } else {
+              console.log(err);
+          }
+  });
 });
 
 module.exports = router;
